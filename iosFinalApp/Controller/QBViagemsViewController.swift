5b92fa30e5df8abf965ff039fedf395e86517a02
DocAdmin <docadmin@lab3710.local>
2018-06-10 21:19:47 +0100

    commit 100620182119

---
+++ iosFinalApp/Controller/QBViagemsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  QBViagemsViewController.swift
+//  iosFinalApp
+//
+//  Created by DocAdmin on 6/10/18.
+//  Copyright © 2018 davidmatos. All rights reserved.
+//
+
+import UIKit
+
+class QBViagemsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    // Outlets
+    // --
+    
+    
+    // --
+    // End Outlets
+    
+    // Actions
+    // --
+    
+    
+    // --
+    // End Actions
+    
+    // Functions
+    // --
+    
+    
+    // --
+    // End Functions
+    
+
+}
